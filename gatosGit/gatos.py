b6eec6c368333a19357f3ce89daac984f42aa0bb
Daniel Alemán <copperdar27@gmail.com>
2023-02-03 12:32:03 -0600

    se crea un objeto gato3 con información diferente a los gatos anteriores

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -12,4 +12,10 @@ gato2.color = "cafe"
 gato2.edad = 3
 gato2.aniadirlista()
 
+gato3 = funcionesTarea.gato()
+gato3.nombre = "Skiper"
+gato3.color = "blanco"
+gato3.edad = 2
+gato3.aniadirlista()
+
 funcionesTarea.printlista()

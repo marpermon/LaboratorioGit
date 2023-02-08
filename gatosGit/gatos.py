ea98f8c4d9c988017075ccf6ddf8e1b785d38fd7
isaac <isaaccamposmon@gmail.com>
2023-02-08 12:54:15 -0600

    Agregar gato3

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -12,4 +12,10 @@ gato2.color = "cafe"
 gato2.edad = 3
 gato2.aniadirlista()
 
+gato3 = funcionesTarea.gato()
+gato3.nombre = "Simon"
+gato3.color = "Amarillo"
+gato3.edad = 2
+gato3.aniadirlista()
+
 funcionesTarea.printlista()
